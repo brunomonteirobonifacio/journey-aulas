c9fa983eb0f0c7a99d5e0f9933afb95d8124b832
Bruno <brunombonifacio@gmail.com>
2024-07-14 21:31:29 -0300

    Aula 2 React finalizada

--- React/frontend/src/pages/create-trip/confirm-trip-modal.tsx
+++ React/frontend/src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,5 +1,6 @@
 import { Mail, User, X } from "lucide-react";
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 interface ConfirmTripModalProps {
   closeConfirmTripModal: () => void
@@ -37,9 +38,9 @@ export function ConfirmTripModal({
                 <input type="email" name="email" placeholder="Seu e-mail pessoal" className="bg-transparent text-lg text-zinc-400 outline-none flex-1" />
               </div>
               
-              <button type="submit" className="w-full justify-center bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <Button type="submit" size="full">
                 Confirmar criação da viagem
-              </button>
+              </Button>
             </form>
 
           </div>
